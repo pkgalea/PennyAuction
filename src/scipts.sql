d766411bbb8f2f80e1f04ff9f174962482e3f373
= <=>
2020-02-04 10:29:34 -0600

    prevusers

--- src/scipts.sql
+++ src/scipts.sql
@@ -1,20 +1,56 @@
 
-Create maxbids as
-  Select auctionid, username, max(bid) as max_bid from bids group by auctionid, username order by auctionid, max(bid)
+CREATE TABLE bids
+(
+  bidid serial NOT NULL,
+  auctionid integer NOT NULL,
+  bid integer NOT NULL,
+  username character varying(20) NOT NULL,
+  is_bidomatic boolean NOT NULL,
+  CONSTRAINT qauctionid_bid_unique UNIQUE (auctionid, bid)
+);
+
+CREATE TABLE auctions
+(
+  auctionid serial NOT NULL , 
+  qauctionid integer NOT NULL ,
+  cardvalue integer NOT NULL ,
+  bidvalue integer NOT NULL ,
+  tracking double precision NOT NULL ,
+  auctiontime timestamp without time zone NOT NULL ,
+  limited_allowed boolean NOT NULL ,
+  cashvalue integer NOT NULL ,
+  cardtype character varying(20) NOT NULL ,
+  runtime integer NOT NULL ,
+  lock_price integer NOT NULL ,
+  CONSTRAINT qauctionid_unique UNIQUE (qauctionid)
+);
 
 
+CREATE INDEX auctionid_idx ON public.bids USING btree (auctionid);
+CREATE INDEX bid_idx ON public.bids USING btree (bid);
+CREATE INDEX bidomatic_idx ON public.bids USING btree (is_bidomatic);
+CREATE INDEX username_idx ON public.bids USING btree (username);
 
+
+with bozo as
+(
 Select bids.auctionid, cardvalue, auctiontime, bidvalue, cardtype, limited_allowed, bid, username, is_bidomatic, 
        bid >= lock_price as is_locked,
-	   bid=MAX(bid) OVER (PARTITION BY bids.auctionid) AS is_winner,
-	   bid=MAX(bid) OVER (PARTITION BY bids.auctionid)-1 AS is_pen,
 	   MAX(bid) OVER (PARTITION BY bids.auctionid) AS eventual_win_price,
            Sum(1) OVER (PARTITION by bids.auctionid, username ORDER BY bid) as bids_so_far,
-           count(*) OVER (PARTITION BY bids.auctionid, username) as eventual_bids, 
-           bid=max(bid) OVER (PARTITION by bids.auctionid, username) and not bid=MAX(bid) OVER (PARTITION BY bids.auctionid) as giveup,
-           (CASE WHEN is_bidomatic THEN Sum(1) OVER (PARTITION by bids.auctionid, username, is_bidomatic ORDER BY bid)
-		ELSE  Sum(1) OVER (PARTITION by bids.auctionid, username ORDER BY bid) - sum(1) OVER (PARTITION by bids.auctionid, username, is_bidomatic ORDER BY bid) END)
-		as bidomatic_bids_so_far
-           from bids  INNER JOIN Auctions on Auctions.qauctionid = bids.auctionID WHERE bids.auctionid = 783108868 
-           order by auctionId, bid;
-        
+           count(*) OVER (PARTITION BY bids.auctionid, username) as eventual_bids,
+            Sum(1) OVER (PARTITION by bids.auctionid, username, is_bidomatic ORDER BY bid) as bids_of_this_type,
+	   row_number() over (partition by username, bids.auctionid order by bid) -
+	   row_number() over (partition by username, is_bidomatic, bids.auctionid order by bid) as prestreak, 
+	   (CASE WHEN row_number() over (partition by bids.auctionid, username order by bid)=1 THEN 1 ELSE 0 END) as debut
+           from bids  INNER JOIN Auctions on Auctions.qauctionid = bids.auctionID 
+          /* WHERE bids.auctionID = 100653479*/
+ )
+ select bozo.*, 
+ row_number() over (partition by prestreak, auctionid, username order by bid) as streak,
+ sum(debut) over (partition by auctionid order by bid)-1 as prevusers,
+ bid=eventual_win_price as is_winner,
+ bid=eventual_win_price -1 as is_pen,
+ bid=eventual_bids and bid != eventual_win_price as giveup,
+ (CASE when is_bidomatic THEN bids_of_this_type ELSE bids_so_far-bids_of_this_type END) as bom_bids_so_far
+  from bozo order by auctionid, bid

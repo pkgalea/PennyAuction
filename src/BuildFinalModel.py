390d16fa136618f01f643b888190d53dbe661f07
= <=>
2020-03-03 13:42:02 -0600

    BuildFinalModel

--- src/BuildFinalModel.py
+++ src/BuildFinalModel.py
@@ -2,6 +2,8 @@ import BuildModel
 import PrevInfo
 import psycopg2 as pg2
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import pickle
 
 print ("Connecting to SQL")
 conn = pg2.connect(user='postgres',  dbname='penny', host='localhost', port='5432', password='password')
@@ -16,7 +18,7 @@ y = df['is_winner']
 model = RandomForestClassifier(n_estimators=200, n_jobs=-1)
 
 pm = BuildModel.PennyModel(model, is_regressor=False,sampling_ratio=.5 )
-pm.fit(X_train, y_train)
+pm.fit(X, y)
 pm.pickle("rf.pkl")
 pi = PrevInfo()
 pi.pickle("pi.pkl")

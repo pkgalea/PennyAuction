160cb8bcc2bab3152827b1fbd2fa25ecc9df536b
= <=>
2020-03-03 09:57:31 -0600

    commented

--- src/BidTrackerScraper.py
+++ src/BidTrackerScraper.py
@@ -6,8 +6,23 @@ from backports import configparser
 import sys
 
 class BidTrackerScraper:
-    
+    """ 
+    Scrapes the BidTracker.com website to get the latest raw auction data into MongoDB.
+      
+    Attributes: 
+        pages_collection (MongoDB collection): The collection of raw html pages for each auction
+        _username(str): The bidtracker username read from the config file
+        _password(str): The bidtracker password file read from the config file
+        _auction_pages(dict): Dictionary of the different types of auction to be scraped (e.g. Amazon 50 + 30 bids)
+        _session: The requests session for the scraper
+    """
     def __init__ (self):
+        """ 
+        The constructor for BidTrackerScraper class. 
+  
+        Parameters: None  
+        Returns: None  
+        """
         self.pages_collection = None
         self._username = None
         self._password = None
@@ -15,11 +30,23 @@ class BidTrackerScraper:
         self._session = requests.Session()
     
     def _connect_to_mongodb(self):     
+        """ 
+        Connects to Mongo Database and gets the pages collection.
+  
+        Parameters: None  
+        Returns: None  
+        """
         myclient = pymongo.MongoClient("mongodb://localhost:27017/")
         db = myclient["penny"]
         self.pages_collection = db["pages"]
 
     def _read_config(self):
+        """ 
+        Reads the Bidtracker username/password and which auctions to read from the config file.
+  
+        Parameters: None  
+        Returns: None  
+        """        
         config = configparser.ConfigParser() 
         configFilePath = 'PennyAuctions.config'
         config.read(configFilePath)
@@ -27,7 +54,14 @@ class BidTrackerScraper:
         self._password = config['login-info']['password']
         self._auction_pages = dict(config['auction-pages'])        
 
-    def login(self, sleep_time):
+    def login(self, sleep_time=1):
+         """ 
+        Uses requests to log into bid tracker class.  
+  
+        Parameters: 
+            sleep_time(float): How many seconds to sleep after logging in (to prevent server overload)
+        Returns: None  
+        """       
         self._read_config()
         payload = {}     
         r = self._session.get("https://www.bidtracker.info/Account/Login" )
@@ -36,14 +70,16 @@ class BidTrackerScraper:
         vs = soup.find(id='__VIEWSTATE')
         vsg = soup.find(id='__VIEWSTATEGENERATOR')
         ev = soup.find(id='__EVENTVALIDATION')
-        payload['__EVENTTARGET']=''
-        payload['__EVENTARGUMENT']='' 
-        payload['__VIEWSTATE']=vs["value"]
-        payload['__VIEWSTATEGENERATOR'] = vsg["value"]
-        payload['__EVENTVALIDATION'] = ev["value"]
-        payload['ctl00$MainContent$UserName'] = self._username
-        payload['ctl00$MainContent$Password'] = self._password
-        payload['ctl00$MainContent$ctl05'] = "Log+in"
+        payload = { 
+            '__EVENTTARGET': '',
+            '__EVENTARGUMENT': '',
+            '__VIEWSTATE': vs["value"],
+            '__VIEWSTATEGENERATOR': vsg["value"],
+            '__EVENTVALIDATION': ev["value"],
+            'ctl00$MainContent$UserName': self._username,
+            'ctl00$MainContent$Password': self._password,
+            'ctl00$MainContent$ctl05'] = "Log+in"
+        }
 
         time.sleep(sleep_time)
         post = self._session.post("http://www.bidtracker.info/Account/Login", data=payload)
@@ -52,6 +88,12 @@ class BidTrackerScraper:
         time.sleep(sleep_time)
 
     def _get_all_auction_ids_for_group(self, auction_group):
+         """ 
+        Returns a list of all the auctionIds for a particular auction group (e.g. Amazon $50 + 30 bids)
+  
+        Parameters: auction_group (str): The auction group to be read(e.g. Amazon $50 + 30 bids)  
+        Returns: auction_ids (list): The list of all the auction_ids as strings 
+        """       
         url = "http://www.bidtracker.info/AllAuctionsByDesc?hash="+self._auction_pages[auction_group]+"&site=quibids"
         r = self._session.get(url)
         split = r.text.split("auction_id=")
@@ -62,6 +104,16 @@ class BidTrackerScraper:
     
     
     def _scrape_auction(self, aID, auction_group, sleep_time=1):
+         """ 
+        Scrapes all three relevant auction pages for a specific auction
+
+        Parameters:
+            aID(str): The id of the auction to be scraped
+            auction_group(str): The group of the auction ("e.g. Amazon $50 + 30 bids)
+            sleep_time(float): The number of seconds to sleep between each scrape.
+        Returns: 
+            page_dict(dict): A dictionary of the three raw html pages that were scraped (Auction, AuctionTable, AuctionHistory)
+        """       
         page_dict = {"_id": aID, "AuctionGroup": auction_group}
         time.sleep(sleep_time)
         r = self._session.get("http://www.bidtracker.info/Auction?site=quibids&auction_id=" + aID)  
@@ -78,6 +130,15 @@ class BidTrackerScraper:
 
 
     def _scrape_auction_group(self, auction_group, break_after):
+         """ 
+        Scrapes all auctions for an auction group (e.g. Amazon $50 + 30)
+  
+        Parameters: 
+            auction_group (str): The group to be scraped.
+            break_after (int): The number of scrapes after which you should quit.
+        Returns: 
+            None  
+        """       
         i = 0
         auction_ids = self._get_all_auction_ids_for_group(auction_group)
         for aID in auction_ids:
@@ -90,12 +151,15 @@ class BidTrackerScraper:
             if i==break_after:
                 break
 
-    def single_scrape(self,auctionID):
-        with requests.Session() as self._session:
-            self.login(0)
-            return self._scrape_auction(auctionID, "whatever", 0)
 
     def scrape(self, break_after):
+        """ 
+        Reads the config file, logs into Bidtracker.com, and then proceeds to scape all auction groups.
+  
+        Parameters: 
+            break_after (int): The number of scrapes after which you should quit.
+        Returns: None  
+        """        
         self._connect_to_mongodb()
         with requests.Session() as self._session:
             self.login(3)
@@ -103,6 +167,13 @@ class BidTrackerScraper:
                 self._scrape_auction_group(auction_group, break_after)
             
     def scrape_upcoming_auctions(self):
+        """ 
+        Scapes the page for immediately upcoming auctions (used by the live tracker)
+
+        Parameters: None
+        Returns:
+            str: The raw HTML for the upcoming auctions
+        """        
         r = self._session.get("http://www.bidtracker.info/AllAuctionsTable?site=quibids&future=true")
         return r.text
  

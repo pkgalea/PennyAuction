60fc6f0d20b00d72c4ee9592568e260a5b6892c7
Ubuntu <ubuntu@ip-172-31-32-109.us-east-2.compute.internal>
2020-07-28 15:05:14 +0000

    Rename folders

--- src/pull_models.sh
+++
@@ -1,3 +0,0 @@
-aws s3 cp s3://pennys3storage/rf.pkl rf.pkl
-aws s3 cp s3://pennys3storage/pi.pkl pi.pkl
-
--- src/push_models.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-aws s3 cp rf.pkl  s3://pennys3storage/rf.pkl
-aws s3 cp pi.pkl  s3://pennys3storage/pi.pkl 
-
